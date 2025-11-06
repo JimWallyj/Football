2f7bc4842e119339e1e9b6f7d8fdd32256fa9851
Jim Walejko <jwalejko@me.com>
2025-11-05 20:32:11 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Jim Walejko on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
